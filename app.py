c1f9b516d052cfdabc60ce7b94fef1215c6cb8f8
DikoJoseitu <espiritujoseph008@gmail.com>
2026-03-09 06:41:24 +0800

    Update app.py

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def get_db_connection():
         host=os.environ.get("DB_HOST"),
         user=os.environ.get("DB_USER"),
         password=os.environ.get("DB_PASSWORD"),
-        database=os.environ.get("DB_NAME", "NCEMS"),
+        database=os.environ.get("DB_NAME", "ncems"),
         port=int(os.environ.get("DB_PORT", 3306))
     )
     return conn
